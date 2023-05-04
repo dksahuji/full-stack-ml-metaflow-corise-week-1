edeeb2366014f475a473b279d33e777c3b000c7a
Devendra Kumar Sahu <dksahuji@gmail.com>
2023-05-04 06:20:46 +0000

    use of custom skip decorator

--- flows/local/flow_template.py
+++ flows/local/flow_template.py
@@ -4,7 +4,31 @@ Template for writing Metaflows
 
 """
 
-from metaflow import FlowSpec, step, current, card
+from metaflow import FlowSpec, step, current, card, Parameter
+from functools import wraps
+
+class skip:
+    """
+    A Flow decorator to skip a step in flow by checking on the check argument
+    Args:
+        check: condition to check for whether to skip the current step,
+        condition have to be set to self attribute.
+        next: name of the next step, after skipping
+    """
+
+    def __init__(self, check: str, next: str) -> None:
+        self.check = check
+        self.next = next
+
+    def __call__(self, f):
+        @wraps(f)
+        def func(s):
+            if getattr(s, self.check):
+                s.next(getattr(s, self.next))
+            else:
+                return f(s)
+        return func
+
 
 
 class Template_Flow(FlowSpec):
@@ -19,6 +43,8 @@ class Template_Flow(FlowSpec):
     - Model training
     - Model deployment
     """
+    
+    to_skip = Parameter("to_skip", default=False, type=bool)
 
     @card
     @step
@@ -49,7 +75,7 @@ class Template_Flow(FlowSpec):
         """
         print("In this step, you'll write your data validation code")
 
-        self.next(self.get_model_config)
+        self.next(self.get_model_config, self.get_another_model_config)
 
     @step
     def get_model_config(self):
@@ -58,9 +84,18 @@ class Template_Flow(FlowSpec):
         """
         print("In this step, you'll configure your model + hyperparameters")
         self.next(self.train_model)
+    
+    @skip(check="to_skip", next="train_model")
+    @step
+    def get_another_model_config(self):
+        """
+        Configure model + hyperparams
+        """
+        print("skip this step. ")
+        self.next(self.train_model)
 
     @step
-    def train_model(self):
+    def train_model(self, inputs):
         """
         Train your model
         """
@@ -76,7 +111,7 @@ class Template_Flow(FlowSpec):
         print("In this step, you'll deploy your model")
 
         self.next(self.end)
-
+    
     @step
     def end(self):
         """
